f647a2ce26280cee22face1efb7efe802b53f6fb
carlosapa <carlos_non86@msn.com>
2015-09-13 12:21:01 +0200

    Add people

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,16 +7,14 @@ var App = function () {
 	root.app = $('#app');
 	root.app_inner = root.app.find('.app_innerarea');
 	root.app_people = root.app.find('.app_person');
+	root.app_add = root.app.find('.app_add');
 
 	root.people_amount = root.app_people.length;
 	root.size = 0;
 
-	root.init = (function () {
+	root.new_person_query = '<div class="app_person"></div>';
 
-		root.position_innerarea();
-		root.position_people();
-
-	})();	
+	root.init();
 };
 
 
@@ -24,6 +22,27 @@ App.prototype.xx = function () {
 	var that = this;
 	
 };
+
+App.prototype.init = function () {
+	var that = this;
+
+	that.position_innerarea();
+	that.position_people();
+	that.init_events();
+};
+
+
+App.prototype.init_events = function () {
+	var that = this;
+
+	that.app_add.each(function (i, e) {
+		$(e).on('click', function () {
+			that.add_person();
+		});
+	});
+	
+};
+
 
 App.prototype.position_innerarea = function () {
 	var that = this;
@@ -43,6 +62,7 @@ App.prototype.position_people = function () {
 
 	that.app_people.each(function (i, e) {
 		
+		//move person
 		var translate = that.size / 2;
 		var angle = (360 / that.people_amount) * i;
 		var transform = 'translate(-50%, calc(-50% - ' + translate + 'px)) rotate(' + angle + 'deg)';
@@ -61,11 +81,43 @@ App.prototype.position_people = function () {
 		  '-o-transform-origin': transform_origin,
 		  'transform-origin': transform_origin
 		});
+
+		//create person UI
+		var person_logo = $('<span class="app_person_logo"></span>');
+		$(e).append(person_logo);
+
+		var contra_angle = 0 - ((360 / that.people_amount) * i);
+		var contra_transform = 'translate(-50%, -50%) rotate(' + contra_angle + 'deg)';
+		var contra_transform_origin = '50% 50%';
+
+		person_logo.css({
+			'webkit-transform': contra_transform,
+		  '-moz-transform': contra_transform,
+		  '-ms-transform': contra_transform,
+		  '-o-transform': contra_transform,
+		  'transform': contra_transform,
+
+		  'webkit-transform-origin': contra_transform_origin,
+		  '-moz-transform-origin': contra_transform_origin,
+		  '-ms-transform-origin': contra_transform_origin,
+		  '-o-transform-origin': contra_transform_origin,
+		  'transform-origin': contra_transform_origin
+		});
+
+
 	});
 };
 
-App.prototype.add = function () {
+App.prototype.add_person = function () {
+	var that = this;
 
+	//create and add
+	var new_person = $(that.new_person_query);
+	that.app_inner.append(new_person);
+	that.people_amount++;
+
+	//update app
+	that.init();
 };
 
 
